a37f8f929ff2b469f81a886647a4ceb740268cd5
Ed Knueppel <edward.knueppel@zywave.com>
2021-11-22 14:10:25 -0600

    adds Total Cost

--- server.js
+++ server.js
@@ -8,23 +8,26 @@ app.set('view engine', 'ejs');
 
 // index page
 var artist_results = [];
-var total_
+
 app.get('/', async function (req, res) {
+    total_cost = 0;
     const itunesSearchUri = 'https://itunes.apple.com/search?media=music&term=%22';
     let search_term = '';
     if (req.query['artist'] === "") {
-        search_term = '';
+        search_term = 'Khalid';
     } else {
         search_term = req.query['artist'];
     }
     console.log('Searching for ' + search_term);
     const url = itunesSearchUri + search_term + '%22&limit=5';
     artist_results = (await axios.get(url)).data['results'];
-    artist_results.forEach(element => {
-        
+    await artist_results.forEach(element => {
+        total_cost += parseFloat(element['trackPrice']);
+        console.log(element['trackPrice'])
     });
     res.render('pages/index', {
-        artist_results: artist_results
+        artist_results: artist_results,
+        total_cost: total_cost
     });
 });
 
